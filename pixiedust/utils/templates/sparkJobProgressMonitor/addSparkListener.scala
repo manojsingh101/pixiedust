7bd6b815f4a0ddeb4847040fe72807b3ac0e32ad
manojsingh101 <manoj.singh101@ibm.com>
2018-12-28 14:36:12 -0500

    Update addSparkListener.scala

--- pixiedust/utils/templates/sparkJobProgressMonitor/addSparkListener.scala
+++ pixiedust/utils/templates/sparkJobProgressMonitor/addSparkListener.scala
@@ -1,38 +1,13 @@
-import org.apache.spark.sql.types._
 import org.apache.spark.scheduler._
-import org.apache.spark._
-import org.apache.spark.TaskEndReason
-import org.apache.spark.JobExecutionStatus
-import org.apache.spark.SparkContext
-import org.apache.spark.executor._
-//import org.apache.spark.metrics.ExecutorMetricType
-import org.apache.spark.rdd.RDDOperationScope
-import org.apache.spark.scheduler.cluster.ExecutorInfo
 import org.apache.spark.storage._
-//import org.apache.spark.util.{Utils, JsonProtocol}
-import org.apache.spark.scheduler.cluster._
-//import org.apache.spark.InternalAccumulator
 
-import collection.JavaConverters._
-import scala.collection.mutable
 import scala.collection.JavaConverters._
-import scala.collection.Map
-import scala.collection.mutable.{ HashMap, HashSet, LinkedHashMap, ListBuffer }
-import java.net._
-import java.io._
-import java.util.{Properties, UUID}
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import org.json4s.DefaultFormats
+import scala.collection.mutable.HashMap
 import org.json4s.JsonAST._
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 
-import com.ibm.pixiedust.ChannelReceiver
-import com.ibm.pixiedust.PixiedustOutputListener
-
-val executorCores = new HashMap[String, Int]
+var executorCores = new HashMap[String, Int]
 @volatile var totalCores: Int = 0
 @volatile var numExecutors: Int = 0
 
@@ -50,7 +25,7 @@ def serializeStagesJSON(stageInfos: Seq[StageInfo]):String = {
         return "[" + stageInfos.sortWith( (s,t) => s.stageId < t.stageId ).map(si => serializeStageJSON(si)).reduce( _ + "," + _ ) + "]"
     }catch{
         case e:Throwable=>{
-            e.printStackTrace();
+            e.printStackTrace()
             return "[]"
         }
     }
@@ -58,7 +33,7 @@ def serializeStagesJSON(stageInfos: Seq[StageInfo]):String = {
 
 def serializeTaskStartJSON(taskInfo: TaskInfo):String = {
     return s"""{
-        "taskId":"${taskInfo.taskId}", 
+        "taskId":"${taskInfo.taskId}",
         "attemptNumber":${taskInfo.attemptNumber},
         "index":${taskInfo.index},
         "launchTime":${taskInfo.launchTime},
@@ -68,10 +43,10 @@ def serializeTaskStartJSON(taskInfo: TaskInfo):String = {
 }
 
 def serializeTaskEndJSON(taskEnd: SparkListenerTaskEnd):String = {
-    var taskInfo = taskEnd.taskInfo
-    var metricsOpt = Option(taskEnd.taskMetrics)
+    val taskInfo = taskEnd.taskInfo
+    val metricsOpt = Option(taskEnd.taskMetrics)
     return s"""{
-        "taskId":"${taskInfo.taskId}", 
+        "taskId":"${taskInfo.taskId}",
         "attemptNumber":${taskInfo.attemptNumber},
         "index":${taskInfo.index},
         "launchTime":${taskInfo.launchTime},
@@ -82,11 +57,12 @@ def serializeTaskEndJSON(taskEnd: SparkListenerTaskEnd):String = {
 }
 
 val __pixiedustSparkListener = new SparkListener{
+
     private val channelReceiver = new ChannelReceiver()
 
     def setChannelListener(listener:PixiedustOutputListener){
-		channelReceiver.setChannelListener(listener)
-	}
+        channelReceiver.setChannelListener(listener)
+    }
 
     override def onJobStart(jobStart: SparkListenerJobStart) {
         channelReceiver.send("jobStart", s"""{
@@ -108,7 +84,7 @@ val __pixiedustSparkListener = new SparkListener{
     }
 
     override def onTaskStart(taskStart: SparkListenerTaskStart) {
-        
+
         channelReceiver.send("taskStart", s"""{
             "stageId":"${taskStart.stageId}",
             "taskInfo":${serializeTaskStartJSON(taskStart.taskInfo)}
@@ -126,35 +102,31 @@ val __pixiedustSparkListener = new SparkListener{
         """)
     }
 
-    override def onStageSubmitted(stageSubmitted: SparkListenerStageSubmitted) { 
+    override def onStageSubmitted(stageSubmitted: SparkListenerStageSubmitted) {
         channelReceiver.send("stageSubmitted", s"""{
             "stageInfo": ${serializeStageJSON(stageSubmitted.stageInfo)}
         }
         """)
     }
 
-    override def onTaskGettingResult(taskGettingResult: SparkListenerTaskGettingResult) { 
+    override def onTaskGettingResult(taskGettingResult: SparkListenerTaskGettingResult) {
         //channelReceiver.send("taskGettingResult", s"taskGettingResult ${taskGettingResult.taskInfo.taskId} : ${taskGettingResult.taskInfo.executorId}")
     }
 
-    //override def onExecutorMetricsUpdate(executorMetricsUpdate: SparkListenerExecutorMetricsUpdate) { 
-        //System.out.println(s"MetricsUpdate ${executorMetricsUpdate.execId} : ${executorMetricsUpdate.taskMetrics}")
-    //}
-
-    override def onStageCompleted(stageCompleted: SparkListenerStageCompleted) { 
+    override def onStageCompleted(stageCompleted: SparkListenerStageCompleted) {
         channelReceiver.send("stageCompleted", s"""{
             "stageInfo":${serializeStageJSON(stageCompleted.stageInfo)}
         }
         """)
     }
-    
+
     /** Called when an executor is added. */
     override def onExecutorAdded(executorAdded: SparkListenerExecutorAdded) {
         executorCores(executorAdded.executorId) = executorAdded.executorInfo.totalCores
         totalCores += executorAdded.executorInfo.totalCores
         numExecutors += 1
 
-        var executorInfoJson = s"""{
+        val executorInfoJson = s"""{
             "host" : "${executorAdded.executorInfo.executorHost}",
             "numCores" : "${executorAdded.executorInfo.totalCores}",
             "totalCores" : "${totalCores}"
@@ -173,7 +145,7 @@ val __pixiedustSparkListener = new SparkListener{
         totalCores -= executorCores.getOrElse(executorRemoved.executorId, 0)
         numExecutors -= 1
 
-        var executorInfoJson = s"""{
+        val executorInfoJson = s"""{
             "totalCores" : "${totalCores}"
         }"""
 
@@ -182,96 +154,71 @@ val __pixiedustSparkListener = new SparkListener{
             "time" : "${executorRemoved.time}",
             "executorInfo" : "${executorInfoJson}"
         }
-        """)	    
+        """)
     }
 
-   override def onExecutorMetricsUpdate(metricsUpdate: SparkListenerExecutorMetricsUpdate) {
-	val executorId = metricsUpdate.execId
-	val accumUpdates = metricsUpdate.accumUpdates
-	//val executorMetrics = render(metricsUpdate.executorUpdates.map(executorMetricsToJson(_)))
-	val executorMetrics = render(executorMetricsUpdateToJson(metricsUpdate))
+    override def onExecutorMetricsUpdate(metricsUpdate: SparkListenerExecutorMetricsUpdate) {
+        val executorId = metricsUpdate.execId
+        val accumUpdates = metricsUpdate.accumUpdates
+        val executorMetrics = render(executorMetricsUpdateToJson(metricsUpdate))
         channelReceiver.send("executorMetricsUpdate", s"""{
             "executorId":"${executorId}",
             "executorMetricsInfo" : "${executorMetrics}"
-        }	
-        """)	   
-   }
-	
-  def executorMetricsUpdateToJson(metricsUpdate: SparkListenerExecutorMetricsUpdate): JValue = {
-    val execId = metricsUpdate.execId
-    val accumUpdates = metricsUpdate.accumUpdates
-    ("Event" -> "SparkListenerExecutorMetricsUpdate") ~
-    ("Executor ID" -> execId) ~
-    ("Metrics Updated" -> accumUpdates.map { case (taskId, stageId, stageAttemptId, updates) =>
-      ("Task ID" -> taskId) ~
-      ("Stage ID" -> stageId) ~
-      ("Stage Attempt ID" -> stageAttemptId) ~
-      ("Accumulator Updates" -> JArray(updates.map(accumulableInfoToJson).toList))
-    })
-  }
+        }
+        """)
+    }
 
-  def accumulableInfoToJson(accumulableInfo: AccumulableInfo): JValue = {
-    val name = accumulableInfo.name
-    ("ID" -> accumulableInfo.id) ~
-    ("Name" -> name) ~
-    ("Update" -> accumulableInfo.update.map { v => accumValueToJson(name, v) }) ~
-    ("Value" -> accumulableInfo.value.map { v => accumValueToJson(name, v) })
-  }	
-  //("Count Failed Values" -> accumulableInfo.countFailedValues) ~
-  //("Metadata" -> accumulableInfo.metadata)
-	
- def accumValueToJson(name: Option[String], value: Any): JValue = {
-    if (name.exists(_.startsWith("internal.metrics."))) {
-      value match {
-        case v: Int => JInt(v)
-        case v: Long => JInt(v)
-        // We only have 3 kind of internal accumulator types, so if it's not int or long, it must be
-        // the blocks accumulator, whose type is `java.util.List[(BlockId, BlockStatus)]`
-        case v =>
-          JArray(v.asInstanceOf[java.util.List[(BlockId, BlockStatus)]].asScala.toList.map {
-            case (id, status) =>
-              ("Block ID" -> id.toString) ~
-              ("Status" -> blockStatusToJson(status))
+    def executorMetricsUpdateToJson(metricsUpdate: SparkListenerExecutorMetricsUpdate): JValue = {
+        val execId = metricsUpdate.execId
+        val accumUpdates = metricsUpdate.accumUpdates
+        ("Event" -> "SparkListenerExecutorMetricsUpdate") ~
+          ("Executor ID" -> execId) ~
+          ("Metrics Updated" -> accumUpdates.map { case (taskId, stageId, stageAttemptId, updates) =>
+              ("Task ID" -> taskId) ~
+                ("Stage ID" -> stageId) ~
+                ("Stage Attempt ID" -> stageAttemptId) ~
+                ("Accumulator Updates" -> JArray(updates.map(accumulableInfoToJson).toList))
           })
-      }
-    } else {
-      // For all external accumulators, just use strings
-      JString(value.toString)
     }
-  }
-	
-  def blockStatusToJson(blockStatus: BlockStatus): JValue = {
-    val storageLevel = storageLevelToJson(blockStatus.storageLevel)
-    ("Storage Level" -> storageLevel) ~
-    ("Memory Size" -> blockStatus.memSize) ~
-    ("Disk Size" -> blockStatus.diskSize)
-  }
 
-  def storageLevelToJson(storageLevel: StorageLevel): JValue = {
-    ("Use Disk" -> storageLevel.useDisk) ~
-    ("Use Memory" -> storageLevel.useMemory) ~
-    ("Deserialized" -> storageLevel.deserialized) ~
-    ("Replication" -> storageLevel.replication)
-  }
-	
-/*	
-  def accumulablesToJson(accumulables: Traversable[AccumulableInfo]): JArray = {
-    JArray(accumulables
-        .filterNot(_.name.exists(accumulableBlacklist.contains))
-        .toList.map(accumulableInfoToJson))
-  }
-*/
-
-	
-  /** Convert executor metrics to JSON. */
-  /**
-  def executorMetricsToJson(executorMetrics: ExecutorMetrics): JValue = {
-    val metrics = ExecutorMetricType.metricToOffset.map { case (m, _) =>
-      JField(m.asInstanceOf[String], executorMetrics.getMetricValue(m))
+    def accumulableInfoToJson(accumulableInfo: AccumulableInfo): JValue = {
+        val name = accumulableInfo.name
+        ("ID" -> accumulableInfo.id) ~
+          ("Name" -> name) ~
+          ("Update" -> accumulableInfo.update.map { v => accumValueToJson(name, v) }) ~
+          ("Value" -> accumulableInfo.value.map { v => accumValueToJson(name, v) })
     }
-    JObject(metrics.toSeq: _*)
-  }
-  */
+
+    def accumValueToJson(name: Option[String], value: Any): JValue = {
+        if (name.exists(_.startsWith("internal.metrics."))) {
+            value match {
+                case v: Int => JInt(v)
+                case v: Long => JInt(v)
+                case v =>
+                    JArray(v.asInstanceOf[java.util.List[(BlockId, BlockStatus)]].asScala.toList.map {
+                        case (id, status) =>
+                            ("Block ID" -> id.toString) ~
+                              ("Status" -> blockStatusToJson(status))
+                    })
+            }
+        } else {
+            JString(value.toString)
+        }
+    }
+
+    def blockStatusToJson(blockStatus: BlockStatus): JValue = {
+        val storageLevel = storageLevelToJson(blockStatus.storageLevel)
+        ("Storage Level" -> storageLevel) ~
+        ("Memory Size" -> blockStatus.memSize) ~
+        ("Disk Size" -> blockStatus.diskSize)
+    }
+
+    def storageLevelToJson(storageLevel: StorageLevel): JValue = {
+        ("Use Disk" -> storageLevel.useDisk) ~
+        ("Use Memory" -> storageLevel.useMemory) ~
+        ("Deserialized" -> storageLevel.deserialized) ~
+        ("Replication" -> storageLevel.replication)
+    }
+
 }
-
 sc.addSparkListener(__pixiedustSparkListener)
