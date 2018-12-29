d4cd3470296757bb08bac32337938fa992f02135
manojsingh101 <manoj.singh101@ibm.com>
2018-12-28 22:34:40 -0500

    Update jobEnded.js

--- pixiedust/utils/templates/sparkJobProgressMonitor/jobEnded.js
+++ pixiedust/utils/templates/sparkJobProgressMonitor/jobEnded.js
@@ -3,6 +3,8 @@ $("#menu{{prefix}}{{jobId}} [id^=status{{prefix}}]").each(function(){
         $(this).text("Skipped");
     }
 })
+$("#pm_overallNumCores{{prefix}}").text("{{totalCores}}");
+$("#pm_overallNumExecutors{{prefix}}").text("{{numExecutors}}");
 
 var n = $("#pm_overallProgress{{prefix}}");
 n.attr("value", parseInt(n.attr("max")))
