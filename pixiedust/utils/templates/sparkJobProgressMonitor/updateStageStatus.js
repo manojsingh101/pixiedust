064b1e677e627e647580225008b6c17549efc58e
manojsingh101 <manoj.singh101@ibm.com>
2018-12-28 22:33:14 -0500

    Update updateStageStatus.js

--- pixiedust/utils/templates/sparkJobProgressMonitor/updateStageStatus.js
+++ pixiedust/utils/templates/sparkJobProgressMonitor/updateStageStatus.js
@@ -1,4 +1,6 @@
 $("#status{{prefix}}{{stageId}}").text("{{status}}")
+$("#pm_overallNumCores{{prefix}}").text("{{totalCores}}");
+$("#pm_overallNumExecutors{{prefix}}").text("{{numExecutors}}");
 
 {% if host%}
 $("#host{{prefix}}{{stageId}}").text("{{host}}")
