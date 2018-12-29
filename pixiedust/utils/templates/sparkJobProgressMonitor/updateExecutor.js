08cef85107a5de5681c303465d304504bd3fc0ba
manojsingh101 <manoj.singh101@ibm.com>
2018-12-28 22:32:58 -0500

    Update updateExecutor.js

--- pixiedust/utils/templates/sparkJobProgressMonitor/updateExecutor.js
+++ pixiedust/utils/templates/sparkJobProgressMonitor/updateExecutor.js
@@ -1,3 +1,2 @@
 $("#pm_overallNumCores{{prefix}}").text("{{totalCores}}");
 $("#pm_overallNumExecutors{{prefix}}").text("{{numExecutors}}");
-$("#pm_overallJobName{{prefix}}").text("jobname{{totalCores}}:{{numExecutors}}");
