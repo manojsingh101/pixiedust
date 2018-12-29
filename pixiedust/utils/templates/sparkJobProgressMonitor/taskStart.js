b2eac65a52403877a2f6324f90752e63f1a6c4f3
manojsingh101 <manoj.singh101@ibm.com>
2018-12-28 22:33:25 -0500

    Update taskStart.js

--- pixiedust/utils/templates/sparkJobProgressMonitor/taskStart.js
+++ pixiedust/utils/templates/sparkJobProgressMonitor/taskStart.js
@@ -5,3 +5,5 @@ function incProgress(id){
 incProgress("#progress{{prefix}}{{data["stageId"]}}");
 incProgress("#pm_overallProgress{{prefix}}");
 $("#progressNumTask{{prefix}}{{data["stageId"]}}").text("{{data["taskInfo"]["index"]+1}}");
+$("#pm_overallNumCores{{prefix}}").text("{{totalCores}}");
+$("#pm_overallNumExecutors{{prefix}}").text("{{numExecutors}}");
