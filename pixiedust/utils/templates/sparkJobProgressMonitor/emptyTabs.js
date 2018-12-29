cd260eb97708ebf81b4c4b5898583e5699c6d6a4
manojsingh101 <manoj.singh101@ibm.com>
2018-12-28 22:35:27 -0500

    Update emptyTabs.js

--- pixiedust/utils/templates/sparkJobProgressMonitor/emptyTabs.js
+++ pixiedust/utils/templates/sparkJobProgressMonitor/emptyTabs.js
@@ -1,5 +1,3 @@
 $("#progressMonitors{{prefix}}").empty();
 $("#tabContent{{prefix}}").empty();
 
-$("#pm_overallNumCores{{prefix}}").text("{{totalCores}}");
-$("#pm_overallNumExecutors{{prefix}}").text("{{numExecutors}}");
